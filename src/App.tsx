b4c4e2a8f5793b92d26a4fb763ecf874c931233c
dragonfirecmv <dragonfirecmv@hotmail.com>
2020-06-22 21:04:13 +0700

    first commit (template)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { h, render } from 'preact';
+import { MenuBar } from './components/MenuBar/MenuBar'
+
+export const App = () => {
+
+  return (
+    <div>
+      <MenuBar/>
+      Hello mate!
+    </div>
+  )
+}
---
+++ src/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,4 @@
+import { h, render } from 'preact';
+import './menu-bar.scss'
+
+export const MenuBar = () => <div id="menu-bar">WooHoo</div>
