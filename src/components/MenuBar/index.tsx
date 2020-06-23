6f7f0f0de7e613f075bbcf229fb519bf466c6822
dragonfirecmv <dragonfirecmv@hotmail.com>
2020-06-23 20:59:53 +0700

    Menu Bar: added logo + layout improvements

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,11 +1,19 @@
 import { h } from 'preact';
+import { Container } from 'components/Container'
+import SiteLogoEN from "assets/images/en-site-logo.png"
 import './menu-bar.scss'
 
 export const MenuBar = () => {
 
   return (
     <div className="menu-bar">
-      Hello
+      <Container>
+        <div className="contents">
+          <div className="area-logo">
+            <img src={SiteLogoEN}/>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
