a4b0a6c1221270c23d64e4f58618184d43b96765
dragonfirecmv <dragonfirecmv@hotmail.com>
2020-06-23 20:59:16 +0700

    expanded Billboard functionality (background layout)

--- src/components/Billboard/index.tsx
+++ src/components/Billboard/index.tsx
@@ -1,11 +1,20 @@
 import { h } from 'preact'
+import "./_billboard.scss"
+import img1 from "assets/images/billboard/bb-image-1.jpg"
 
 
 export const Billboard = () => {
 
   return (
-    <div className="billboard">
-      Sample billboard
+    <div className="al-billboard">
+      <div className="al-billboard bkg">
+        <img src={img1}/>
+        <div id="gradient-radial"/>
+        <div id="gradient-linear"/>
+      </div>
+      <div className="al-billboard content">
+        
+      </div>
     </div>
   )
 }
