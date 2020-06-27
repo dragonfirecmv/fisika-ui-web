c325969a25bf91d12e8924ed18c95471c7e73b5d
Al <arudei@Als-MacBook-Pro.local>
2020-06-27 17:11:40 +0700

    new home route

---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import { Billboard } from 'components/Billboard';
+import "./_home.scss"
+
+export const PageHome = () => {
+
+  return (
+    <div className="page-home">
+      <Billboard/>
+    </div>
+  )
+}
